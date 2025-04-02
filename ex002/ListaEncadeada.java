df3ae7b152c56712479833301b4c0c647e0fdf36
Raniel Lopes <raniel.ldms@gmail.com>
2025-04-02 16:53:35 -0300

    criando o metodo ordenar e listar

--- ex002/ListaEncadeada.java
+++ ex002/ListaEncadeada.java
@@ -66,4 +66,56 @@ public class ListaEncadeada {
 			penultimo.setProximo(null);
 		}
 	}
+	
+	//Precisa criar Pesquisar, Listar e colocar em ordem alfabetica
+	
+	public Aluno pesquisar(String nome) {
+		Celula atual = primeiro;
+		while(atual != null) {
+			if(atual.getValor().getNome().equalsIgnoreCase(nome)) {
+				return atual.getValor();
+			}
+			atual = atual.getProximo();
+		}
+		return null; //Retorna null caso não encontre o aluno pesquisado
+	}
+	
+	public void listar() {
+		Celula atual = primeiro;
+		while(atual != null) {
+			System.out.println(atual.getValor().getNome());
+			atual = atual.getProximo();
+		}
+	}
+	
+	public void ordenar() {
+		if(primeiro == null || primeiro.getProximo() == null) {
+			return; // Se a lista estiver vazia ou com um elemento, automaticamente está ordenada
+		}
+		
+		boolean trocou; // Controla se houve trocas nas casinhas
+		do {
+			trocou = false; // Assumimos primeiro que não houve troca
+			Celula atual = primeiro;
+			Celula anterior = null;
+			
+			while(atual.getProximo() != null) { //Percorre a lista até o penúltimo elemento
+				Celula proximo = atual.getProximo();
+				
+				// Comparando os nomes
+				if(atual.getValor().getNome().compareToIgnoreCase(proximo.getValor().getNome()) > 0) {
+					// Se o nome do aluno atual for maior que o próximo, troca a posição
+					
+					if(anterior == null) {
+						// Se estivermos no primeiro elemento da lista
+						primeiro = proximo;
+					} else {
+						// Se não, ajusta o ponteiro do elemento anterior]
+						anterior.setProximo(proximo);
+					}
+					
+				}
+			}
+		}
+	}
 }
